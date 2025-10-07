bb41d03e985bc631a36e430b9a8b8ed6260ab801
Elkouari <elkouariayoub@gmail.com>
2025-10-07 12:15:33 +0100

    Refine KPI chart client wrappers

--- components/AudienceChartClient.tsx
+++ components/AudienceChartClient.tsx
@@ -1,7 +1,9 @@
 "use client"
 
-import KpiChart, { type KpiChartProps } from "@/components/KpiChart"
+import KpiChart, { type KpiChartProps, type KpiPoint } from "./KpiChart"
 
-export default function AudienceChartClient(props: KpiChartProps) {
+export default function AudienceChartClient(
+  props: { data: KpiPoint[] } & Partial<KpiChartProps>,
+) {
   return <KpiChart {...props} />
 }
--- components/EngagementChartClient.tsx
+++ components/EngagementChartClient.tsx
@@ -1,7 +1,9 @@
 "use client"
 
-import KpiChart, { type KpiChartProps } from "@/components/KpiChart"
+import KpiChart, { type KpiChartProps, type KpiPoint } from "./KpiChart"
 
-export default function EngagementChartClient(props: KpiChartProps) {
+export default function EngagementChartClient(
+  props: { data: KpiPoint[] } & Partial<KpiChartProps>,
+) {
   return <KpiChart {...props} />
 }
